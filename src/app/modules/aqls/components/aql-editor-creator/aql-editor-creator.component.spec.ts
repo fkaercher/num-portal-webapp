58cecb61e0ecd83ef7d98533b61f84c7a6b4c3c2
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2024-08-20 12:00:00 +0200

    Add Search mode to AQL builder dialog

--- src/app/modules/aqls/components/aql-editor-creator/aql-editor-creator.component.spec.ts
+++ src/app/modules/aqls/components/aql-editor-creator/aql-editor-creator.component.spec.ts
@@ -159,7 +159,7 @@ describe('AqlEditorCreatorComponent', () => {
     }
 
     it('should open the dialog with the config including the content payload', () => {
-      component.openBuilderDialog()
+      component.openBuilderDialog(AqlBuilderDialogMode.AqlEditor)
       expect(mockDialogService.openDialog).toHaveBeenCalledTimes(1)
       expect(dialogCallParameter.dialogContentComponent).toEqual(
         dialogConfig.dialogContentComponent
@@ -177,7 +177,7 @@ describe('AqlEditorCreatorComponent', () => {
         selectedTemplateIds: ['temp1', 'temp2'],
       }
       beforeEach(() => {
-        component.openBuilderDialog()
+        component.openBuilderDialog(AqlBuilderDialogMode.AqlEditor)
         afterClosedSubject$.next(dialogOutput)
       })
       it('should set the dialog result to the component', () => {
@@ -189,7 +189,7 @@ describe('AqlEditorCreatorComponent', () => {
 
     it('should do nothing, when its not a confirm-close', () => {
       jest.spyOn(component, 'handleDialogConfirm')
-      component.openBuilderDialog()
+      component.openBuilderDialog(AqlBuilderDialogMode.AqlEditor)
       afterClosedSubject$.next()
       expect(component.handleDialogConfirm).not.toHaveBeenCalled()
     })
